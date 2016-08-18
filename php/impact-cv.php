73ca4aa069f401a9536233e4cf5387bad6a806b5
Guillaume Lobet <guillaume.lobet@gmail.com>
2016-08-18 11:36:18 +0200

    Update impact-cv.php

--- php/impact-cv.php
+++ php/impact-cv.php
@@ -6,7 +6,7 @@
     $poster = isset($_POST['poster']);
     
 	// Get the JSON file and decode it
-	$impacturl = "https://v1.impactstory.org/".$user;
+	$impacturl = "https://v1.impactstory.org/profile/".$user;
 	$json_str = file_get_contents($impacturl);
 	$json = json_decode($json_str);
 	
